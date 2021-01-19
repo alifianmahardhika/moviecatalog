feb92722bd25c37abb788920e577bc4b417bf688
Alifian Mahardhika <fiansmamda@gmail.com>
2021-01-19 12:21:33 +0900

    Adding FavoriteMovieFragment.kt and FavoriteTvFragment.kt

---
+++ app/src/main/java/com/example/moviecatalog/ui/movies/FavoriteMovieFragment.kt
@@ -0,0 +1,57 @@
+package com.example.moviecatalog.ui.movies
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.moviecatalog.databinding.FragmentMovieBinding
+import com.example.moviecatalog.viewmodel.ViewModelFactory
+import com.example.moviecatalog.vo.Status
+
+class FavoriteMovieFragment : Fragment() {
+    private lateinit var fragmentMovieBinding: FragmentMovieBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        fragmentMovieBinding = FragmentMovieBinding.inflate(layoutInflater, container, false)
+        return fragmentMovieBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (activity != null) {
+
+            val factory = ViewModelFactory.getInstance(requireActivity())
+            val viewModel = ViewModelProvider(this, factory)[MoviesViewModel::class.java]
+            val movieAdapter = MovieAdapter()
+            viewModel.getMovies().observe(this, { movies ->
+                if (movies != null) {
+                    when (movies.status) {
+                        Status.LOADING -> fragmentMovieBinding.progressBar.visibility = View.VISIBLE
+                        Status.SUCCESS -> {
+                            fragmentMovieBinding.progressBar.visibility = View.GONE
+                            movies.data?.let { movieAdapter.setMovies(it) }
+                            movieAdapter.notifyDataSetChanged()
+                        }
+                        Status.ERROR -> {
+                            fragmentMovieBinding.progressBar.visibility = View.GONE
+                            Toast.makeText(context, "Terjadi kesalahan", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            })
+
+            with(fragmentMovieBinding.rvMovie) {
+                layoutManager = LinearLayoutManager(context)
+                setHasFixedSize(true)
+                adapter = movieAdapter
+            }
+        }
+    }
+}
